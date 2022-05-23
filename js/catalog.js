bdd5ee4550c5f4165ba6bee2d3a82d6234a24f65
adunyt <65284833+adunyt@users.noreply.github.com>
2022-05-23 21:38:38 +0300

    Add files via upload

--- js/catalog.js
+++ js/catalog.js
@@ -1,4 +1,4 @@
 const queryString = window.location.search;
 const urlParams = new URLSearchParams(queryString);
-var siteTitle = document.getElementsByClassName("catalog-title")[0];
+var siteTitle = document.getElementsByClassName("stock-title")[0];
 siteTitle.innerHTML = urlParams.get("c");
