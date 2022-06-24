7e4a2d679a74a65c9377e0b54f257866ab889eca
adunyt <65284833+adunyt@users.noreply.github.com>
2022-06-24 12:01:38 +0300

    Delete js directory

--- js/catalog.js
+++
@@ -1,5 +0,0 @@
-
-const queryString = window.location.search;
-const urlParams = new URLSearchParams(queryString);
-var siteTitle = document.getElementsByClassName("stock-title")[0];
-siteTitle.innerHTML = urlParams.get("c");
